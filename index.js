26b38e86e5c099e7fd9a2a4e2724a18b90c7c98d
253425aditya <vermaaditya8624@gmail.com>
2025-01-03 15:23:14 +0530

    creating getComputerChoice function to take return diffrent values

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log("Hello world");
-alert("jeje");
+function getComputerChoice(){
+    let num = Math.random();
+    num *= 10;
+    num = Math.floor(num%3);
+    
+    if(num === 0){
+        return "Rock";
+    }
+    else if(num === 1){
+        return "paper";
+    }
+    else{
+        return "Scissor";
+    }
+}
+
+for(let i=1;i<=10;i++){
+    console.log(getComputerChoice());
+}
