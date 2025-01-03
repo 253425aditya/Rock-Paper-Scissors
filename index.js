3b0b5991288922e0b294797d6993791740fddf93
253425aditya <vermaaditya8624@gmail.com>
2025-01-03 17:02:24 +0530

    Completed ROCK-PAPER-SCISSOR game hehe

--- index.js
+++ index.js
@@ -2,18 +2,54 @@ function getComputerChoice(){
     let num = Math.random();
     num *= 10;
     num = Math.floor(num%3);
-    
+       
     if(num === 0){
         return "Rock";
     }
     else if(num === 1){
-        return "paper";
+        return "Paper";
     }
     else{
         return "Scissor";
+    } 
+}
+
+function getHumanChoice(){
+    let string = prompt("Enter your choice");
+    if (!string) {
+        return ""; 
+    }
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();;
+}
+
+
+function playRound(c,h){
+    console.log("Computer : " , c);
+    console.log("Human : " , h);
+    if(c === h){
+        console.log("It's a tie!");
+    }
+    else if((c==="Rock" && h==="Scissor") || (c==="Paper" && h==="Rock") || (c==="Scissor" && h==="Paper")){
+        console.log("You lose! " , c , " beats ",  h);
+        computerScore++;
+    }
+    else{
+        console.log("You win! " , h , " beats ",  c);
+        humanScore++;
+    } 
+}
+
+function playGame(){ 
+    for(let i=1;i<=5;i++){
+        let human = getHumanChoice();
+        let computer = getComputerChoice();
+        playRound(computer,human);
     }
 }
 
-for(let i=1;i<=10;i++){
-    console.log(getComputerChoice());
-}
+let humanScore = 0;
+let computerScore = 0;
+
+playGame();
+
+console.log("Final score HUMAN : " , humanScore, "COMPUTER : ",computerScore);
